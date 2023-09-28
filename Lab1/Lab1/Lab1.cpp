d2315c74c45036bd2b709f7d136ad8400cdbc4a4
Egorallo <sea25042003@gmail.com>
2023-09-28 17:49:40 +0300

    initial commit

---
+++ Lab1/Lab1/Lab1.cpp
@@ -0,0 +1,44 @@
+#include <windows.h>
+
+LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow) {
+	WNDCLASS wc = { 0 };
+
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hInstance = hInst;
+	wc.lpszClassName = L"Lab1";
+	wc.lpfnWndProc = WindowProcedure;
+
+	if (!RegisterClassW(&wc)) {
+		return -1;
+	}
+
+	CreateWindowW(L"Lab1", L"My Window", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 500, 500, NULL, NULL, NULL, NULL);
+
+	MSG msg = { 0 };
+	
+	while (GetMessage(&msg, NULL, NULL, NULL)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return 0;
+
+}
+
+LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
+	switch (msg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProcW(hWnd, msg, wp, lp);
+	}
+}
+
+
+
+
